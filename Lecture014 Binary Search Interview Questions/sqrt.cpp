7333d5a229d41090288dca50dd0c96fb629ead6c
Akash7080 <89637983+Akash7080@users.noreply.github.com>
2022-01-10 19:19:00 +0530

    Update sqrt.cpp

--- Lecture014 Binary Search Interview Questions/sqrt.cpp
+++ Lecture014 Binary Search Interview Questions/sqrt.cpp
@@ -1,55 +1,39 @@
 #include<iostream>
 using namespace std;
-
- long long int sqrtInteger(int n) {
+int binarysearch(int n)
+{
+    int s=0;
+    int e=n;
+    int mid=(e+s)/2;
+    int ans=-1;
+    while(s<=e)
+    {
+        int square=mid*mid;
+       
+        if(square==n)
+        {
         
-        int s = 0;
-        int e = n;
-        long long int mid = s + (e-s)/2;
+        return mid;
+        }
         
-        long long int ans = -1;
-        while(s<=e) {
-            
-            long long int square = mid*mid;
-            
-            if(square == n)
-                return mid;
-            
-            if(square < n ){
-                ans = mid;
-                s = mid+1;
-            }
-            else
-            {
-                e = mid - 1;
-            }
-            mid = s + (e-s)/2;
+        if(square<n)
+        {
+            ans=mid;
+            s=mid+1;
         }
-        return ans;
-    }
-
-double morePrecision(int n, int precision, int tempSol) {
-    
-    double factor = 1;
-    double ans = tempSol;
-
-    for(int i=0; i<precision; i++) {
-        factor = factor/10;
-
-        for(double j=ans; j*j<n; j= j+factor ){
-            ans = j;
+        else
+        {
+          e= mid-1;
         }
+         mid=(e+s)/2;
+        
     }
     return ans;
 }
 
-int main() {
-    int n;
-    cout <<" Enter the number " << endl;
-    cin >> n;
-
-    int tempSol = sqrtInteger(n);
-    cout <<" Answer is " << morePrecision(n, 3, tempSol) << endl;
-
-    return 0;
-}
+    int mySqrt(int x) 
+    {
+        binarysearch(x);
+        
+    }
+};
